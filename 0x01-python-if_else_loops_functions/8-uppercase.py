d0061685c2349ba96eea0207e7a210ccd87c9f34
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-05-31 12:52:56 +0200

    Added .gitignore file

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for ch in str:
-        print("{}".format(chr(ord(ch) - 32) if ord(ch) >= ord('a') and ord(ch) <= ord('z') else ch, end=""))
+        print("{}".format(chr(ord(ch) - 32) if ord(ch) >= ord('a') and ord(ch) <= ord('z') else ch), end="")
     print()
