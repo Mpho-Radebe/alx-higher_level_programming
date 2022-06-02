9c3e804b26053165860516bf04c2de1773e96256
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-03 01:58:45 +0200

    Added files to 0x02

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
         stdout.write("Usage: ./100-my_calculator <a> <operator> <b>\n")
         exit(1)
     operator = argv[2]
-    operand1 = int(argv[1])
-    operand2 = int(argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
     if operator == "+":
-        print("{} + {} = {}".format(operand1, operand2, add(operand1, operand2)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == "-":
-        print("{} - {} = {}".format(operand1, operand2, sub(operand1, operand2)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif operator == "*":
-        print("{} * {} = {}".format(operand1, operand2, mul(operand1, operand2)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif operator == "/":
-        print("{} / {} = {}".format(operand1, operand2, div(operand1, operand2)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         stdout.write("Unknown operator. Available operators: +, -, * and /\n")
         exit(1)
