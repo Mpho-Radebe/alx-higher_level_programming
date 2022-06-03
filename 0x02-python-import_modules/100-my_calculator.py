69234d298acb667bfd15058a726e6bf3607ed506
MphoRadebe <99308354+MphoRadebe@users.noreply.github.com>
2022-06-03 02:29:53 +0200

    Modified 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv, stdout, exit
     from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
-        stdout.write("Usage: ./100-my_calculator <a> <operator> <b>\n")
+        print("Usage: ./100-my_calculator <a> <operator> <b>")
         exit(1)
     operator = argv[2]
     a = int(argv[1])
@@ -14,8 +14,8 @@ if __name__ == "__main__":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif operator == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == "\":
-        print("{} \ {} = {}".format(a, b, div(a, b)))
+    elif operator == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        stdout.write("Unknown operator. Available operators: +, -, * and /\n")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
