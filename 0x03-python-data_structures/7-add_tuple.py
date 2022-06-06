b5c8f1a82796ad9473619b2ea0c99a816e00ea84
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-06 22:51:27 +0200

    0x03 Task 6 code added

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    first = tuple_a[0] if len(tuple_a) >= 1 else 0
+    first += tuple_b[0] if len(tuple_b) >= 1 else 0
+
+    second = tuple_a[1] if len(tuple_a) >= 2 else 0
+    second += tuple_b[1] if len(tuple_b) >= 2 else 0
+
+    return (first, second)
