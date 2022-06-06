6dc2f875a7baa475db6af73b4211fd6a357762a8
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-06 21:20:25 +0200

    0x03 Task 4 code added

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy = []
+    if idx >= 0 and idx < len(my_list):
+        for i in range(0, len(my_list)):
+            if i == idx:
+                copy.append(element)
+            else:
+                copy.append(my_list[i])
+    return copy
