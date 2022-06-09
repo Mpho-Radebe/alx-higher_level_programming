aac098ead047dc968f84088395520d44802ed18d
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-09 19:26:18 +0200

    Added code for 0x04 project task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    diff1 = set.difference(set_1, set_2)
-    diff2 = set.difference(set_2, set_1)
-    return list(diff1 + diff2)
+    diff1 = list(set.difference(set_1, set_2))
+    diff2 = list(set.difference(set_2, set_1))
+    return diff1 + diff2
