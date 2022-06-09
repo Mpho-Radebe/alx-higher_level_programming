6d404c72c4fecd1420c0f287cbaa344b9a1eadae
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-09 23:55:41 +0200

    Added code for 0x04 project task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    delete_key = [key for key in a_dictionary.keys() if a_dictionary.get(key) == value]
+    dic = a_dictionary
+    delete_key = [key for key in a_dic.keys() if a_dic.get(key) == value]
     for key in delete_key:
         a_dictionary.pop(key)
     return a_dictionary
