35111189405781476de41a87aad6c420779082c5
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-09 23:30:41 +0200

    Added code for 0x04 project task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    weights = 0
+    weighted_sum = 0
+    for (s, w) in my_list:
+        weighted_sum += s * w
+        weights += w
+    return weighted_sum / weights
