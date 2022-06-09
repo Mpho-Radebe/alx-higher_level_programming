5bbd4d80d51720df5f13498737fe5bfafb4a70f2
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-09 21:40:18 +0200

    Added code for 0x04 project task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max = None
-    for key in a_dictionary.keys():
-        if max is None or a_dictionary.get(max) < a_dictionary.get(key):
-            max = key
-    return key
+    d = a_dictionary
+    max_key = None
+    for key in d.keys():
+        print(max_key, key)
+        if max_key is None or d.get(max_key) < d.get(key):
+            max_key = key
+    return max_key
