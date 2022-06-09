c0061b9e5832fc155b4caf45607aa1da1c5a1ec4
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-09 18:39:14 +0200

    Added code for project 0x04 task 1 and 2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[e**2 for e in row ] for row in matrix]
+    new_matrix = [[e**2 for e in row] for row in matrix]
     return new_matrix
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+def uniq_add(my_list=[]):
+    l1 = my_list
+    n = range(len(my_list))
+    l2 = [l1[i] for i in n if not l1[i] in l1[(i + 1):]]
+    return sum(l2)
