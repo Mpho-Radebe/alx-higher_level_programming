70582d990c270634054382a18e71da3d0eb8140b
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-09 21:18:23 +0200

    Added code for 0x04 project task 8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+def best_score(a_dictionary):
+    max = None
+    for key in a_dictionary.keys():
+        if max is None or a_dictionary.get(max) < a_dictionary.get(key):
+            max = key
+    return key
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    double_dict = dict();
+    double_dict = dict()
     for key in a_dictionary.keys():
         double_dict[key] = a_dictionary.get(key) * 2
     return double_dict
