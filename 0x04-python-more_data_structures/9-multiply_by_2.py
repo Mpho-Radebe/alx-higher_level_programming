40971aefcd8973f5738d38f60daa4651d62af964
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-09 20:25:01 +0200

    Added code for 0x04 project task 8

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    double_dict = dict();
+    for key in a_dictionary.keys():
+        double_dict[key] = a_dictionary.get(key) * 2
+    return double_dict
