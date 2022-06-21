a04213111f8647030286ea7e8d4833fadf0a505d
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-21 04:59:54 +0200

    Added correction for 0x05 task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     index = 0
-    count = 0;
+    count = 0
     while index < x:
         try:
             print("{:d}".format(my_list[index]), end="")
@@ -13,4 +13,3 @@ def safe_print_list_integers(my_list=[], x=0):
         index += 1
     print()
     return count
-
