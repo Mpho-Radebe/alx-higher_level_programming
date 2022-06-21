3822374005a8c513048a8f9c8732c54b1bc6436b
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-21 18:36:15 +0200

    Added correction for 0x06 task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,25 +5,27 @@
 class Square:
 
     """The Square class"""
-    def size(self, value):
-        """Sets the size of a square to the specified size"""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-   
     def __init__(self, size=0):
-        "Instantiate an object of Square class"
+        """Sets the size of a square to the specified size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
     def size(self):
-        """Return the size of a squaire"""
+        """Return the size of a square"""
         return self.__size
+
+    @size.setter
+    def size(self, value):
+        "Instantiate an object of Square class"
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """Returns the area of the square"""
