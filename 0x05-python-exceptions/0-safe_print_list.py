8df716e6574b147ce8a1addde02d2041a25da2bc
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-21 04:15:12 +0200

    Added README.md file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0;
+    count = 0
 
     while count < x:
         try:
-             print(my_list[count], end="")
-             count += 1
-        except:
+            print(my_list[count], end="")
+            count += 1
+        except IndexError:
             break
     print()
     return count
