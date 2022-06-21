17890731b51404ea67b26566156547d1222fe007
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-21 05:11:27 +0200

    Added correction for 0x05 task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,14 +2,13 @@
 def safe_print_division(a, b):
     div = None
     try:
-       div = a / b
+        div = a / b
     except ZeroDivisionError:
         pass
     finally:
         print("Inside result:", end=" ")
-        if div == None:
+        if div is None:
             print("None")
         else:
             print("{}".format(div))
         return div
-
