c213c8d1184a6ddf77377f5dbd9015c614bd8478
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-21 17:55:23 +0200

    Added code for 0x06 task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
 
     """The Square class"""
     def __init__(self, size=0):
-        """The constructor"""
-        if type(size) =! "int":
+        """Intitializes Square object and provide size validation"""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
