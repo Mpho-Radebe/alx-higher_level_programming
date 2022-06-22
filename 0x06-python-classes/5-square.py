32cb0994b638019f29c95a6810b5b90a3d98154e
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-06-22 08:51:29 +0200

    Added code for 0x06 task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Module to contain the Square class"""
+
+
+class Square:
+
+    """The Square class"""
+    def __init__(self, size=0):
+        """Sets the size of a square to the specified size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Return the size of a square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        "Instantiate an object of Square class"
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print ("#" * self.size)
+Square(5).my_print()
