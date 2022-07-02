fa9775df48edcb38a6d007437431e7bf2dd24f43
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-07-02 10:52:13 +0200

    Added code for 0x08 task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module that contains the class Rectangle"""
+
+
+class Rectangle:
+    """Rectangles to a model a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initiates a rectangle object"""
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """A function to get the height of a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """A function to get the width of a rectangle"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
