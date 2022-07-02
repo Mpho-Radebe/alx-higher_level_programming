5921076fe8cf6a08bcb2fd5d1aab05aeb7e363aa
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-07-02 10:53:44 +0200

    Added code for 0x08 task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,6 +16,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """Setter for height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -26,12 +27,12 @@ class Rectangle:
     def width(self):
         """A function to get the width of a rectangle"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
+        """Setter for width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
