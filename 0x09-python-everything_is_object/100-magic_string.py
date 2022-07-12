e18e65b46de4003471341852c36b642d5d418f39
Mpho Radebe <mpontshengradebe02@gmail.com>
2022-07-12 19:17:15 +0200

    Added code for task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.c = 1 if not hasattr(magic_string, "c") else magic_string.c + 1
-    return  ", ".join([("BestSchool") for i in range(magic_string.c)])
+    magic_string.c = (magic_string.c + 1) if hasattr(magic_string, "c") else 1
+    return ", ".join([("BestSchool") for i in range(magic_string.c)])
